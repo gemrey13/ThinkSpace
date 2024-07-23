a114186fb6115b7a409f3286b8085200bb70bb11
gemrey13 <gemreyranola@gmail.com>
2024-07-23 10:08:55 +0800

    ci: connected to atlas mongoDB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { PORT } from "./config.js";
+import mongoose from "mongoose";
+import { PORT, mongoDBURL } from "./config.js";
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port : ${PORT}`);
-})
+
+mongoose.connect(mongoDBURL)
+    .then(() => {
+        console.log('App is connected to database')
+        app.listen(PORT, () => {
+            console.log(`App is listening to port : ${PORT}`);
+        })
+
+    })
+    .catch((error) => {
+        console.error(error)
+    })
--- server/config.js
+++ server/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = "mongodb+srv://root:thinkspace@thinkspacedb.zbkmbwv.mongodb.net/think-spaceDB?retryWrites=true&w=majority&appName=thinkspaceDB";
